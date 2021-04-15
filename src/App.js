a21bff68df918331292892a28a0a621a0d181b34
patil-pavan7 <patilpandu2@gmail.com>
2021-04-15 13:38:44 +0530

    React application after addition of bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {navbar,navbrand}from 'reactstrap'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+<a className="navbar-brand" href="#">Navbar</a>
+
+
+</nav>
     </div>
   );
 }
